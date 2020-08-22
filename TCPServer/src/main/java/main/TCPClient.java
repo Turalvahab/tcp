f3562c031c39710f3c6fbfaf7f7cb91ad00aa9e8
Tural <vahabzada.tural@gmail.com>
2020-08-23 00:16:04 +0400

    updated

--- TCPServer/src/main/java/main/TCPClient.java
+++ TCPServer/src/main/java/main/TCPClient.java
@@ -9,17 +9,19 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
+import util.FileUtility;
 
 /**
  *
  * @author User
  */
 public class TCPClient {
-    public static void main (String[]args) throws IOException{
+    public static void main (String[]args) throws  Exception{
     Socket socket=new Socket("localhost",6789);
     OutputStream outputStream=socket.getOutputStream();
     DataOutputStream dataOutputStream=new DataOutputStream(outputStream);
-    outputStream.write("Salam server necesen".getBytes());
+    byte[] bytes=FileUtility.readBytes("C:\\Users\\User\\Desktop\\screen.png");
+    outputStream.write(bytes);
     socket.close();
     }
 }
--- mavenproject1/src/main/java/main/TCPServer.java
+++ mavenproject1/src/main/java/main/TCPServer.java
@@ -5,12 +5,15 @@
  */
 package main;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import util.FileUtility;
 
 /**
  *
@@ -21,18 +24,45 @@ public class TCPServer {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
-     
+    
+       readAsByte(); 
+    }
+    public static void readAsByte()throws Exception{
+        
+      ServerSocket ourFirstServerSocket=new ServerSocket(6789);
+  while(true){
+      System.out.println("yeni soket ucun ve ya yeni musteri ucun gozleyirem...");
+    Socket connection =ourFirstServerSocket.accept();
+      System.out.println("urra yeni musteri geldi...");
+      ByteBuffer bf=ByteBuffer.allocate(8192);
+      BufferedInputStream inFromClient=new BufferedInputStream(connection.getInputStream());
+    while(true){
+    int b=inFromClient.read();
+    if(b==-1){
+    break;
+    }
+    bf.put((byte)b);
+    
+    }
+   
+    //FileUtility.writeBytes(bf.array()"");
+    }
+    }
+public static void readAsString() throws Exception{
+
 ServerSocket ourFirstServerSocket=new ServerSocket(6789);
   while(true){
       System.out.println("yeni soket ucun ve ya yeni musteri ucun gozleyirem...");
     Socket connection =ourFirstServerSocket.accept();
       System.out.println("urra yeni musteri geldi...");
     InputStream is=connection.getInputStream();
+    
     InputStreamReader reader=new InputStreamReader(is);
     BufferedReader bReader=new BufferedReader(reader);
     
     String messageFromClient=bReader.readLine();
     System.out.println("musteri deyirki"+messageFromClient);
-    }      
+    }   
+   
 }
 }
